c29c37d44df06a09b45becb6c664aaac75e0e621
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-06-22 18:38:43 +0200

    (hour 1) Started working on layout
    Added dept colors to tailwind config
    
    Added lato font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dept: {
+          50: "#F5F5F5",
+          100: "#E9E9E9",
+          200: "#D9D9D9",
+          300: "#C4C4C4",
+          400: "#9D9D9D",
+          900: "#000000",
+        },
+        primary: "#5115F7",
+      },
+      fontFamily: {
+        sans: ["var(--font-lato)"],
       },
     },
   },
   plugins: [],
-}
+};
